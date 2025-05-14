7374371b61d85003341b109b2138ae9819e33cf4
Springlezz <maria.bursyanina@yandex.ru>
2025-05-14 12:41:04 +0700

    Индекс перенесен, связь с React

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 8080;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 app.listen(PORT, () => {
